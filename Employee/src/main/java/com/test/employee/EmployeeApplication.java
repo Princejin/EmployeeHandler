157ee777e21b09a5f976218d20668f11bc560a14
Princejin <abhisheksrivastava663@gmail.com>
2022-11-17 17:17:44 +0530

    update using swagger

--- Employee/src/main/java/com/test/employee/EmployeeApplication.java
+++ Employee/src/main/java/com/test/employee/EmployeeApplication.java
@@ -2,10 +2,8 @@ package com.test.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
